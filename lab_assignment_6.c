f70f87d6f6c22837bda788208b69d94c5c049e75
DaneS1112 <136855034+DaneS1112@users.noreply.github.com>
2023-06-23 17:12:03 -0400

    Update lab_assignment_6.c

--- lab_assignment_6.c
+++ lab_assignment_6.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 
 int search(int numbers[], int low, int high, int value) 
-{
-	return -1;
+int binarySearch(int* values, int low, int high, int search) {
+	int mid;
+	if (low < high) {
+		mid = (low + high) / 2;
+		if (search < values[mid]) {
+			return binarySearch(values, low, mid - 1, search);
+		}
+		else if (search > values[mid]) {
+			return binarySearch(values, mid + 1, high, search);
+		}
+		else
+			return 1;
+	}
+	return 0;
 }
 
 void printArray(int numbers[], int sz)
